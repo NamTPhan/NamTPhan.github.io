22157360129dc768d490def81ca79aedf658167a
Nam Phan <namtphann@gmail.com>
2020-08-21 23:37:24 +0200

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import NavBar from './components/NavBar'
-import LandingSection from './components/LandingSection'
+import React from "react";
+import NavBar from "./components/NavBar";
+import LandingSection from "./components/LandingSection";
+import AboutSection from "./components/AboutSection";
+import SkillsSection from "./components/SkillsSection";
 
-import 'antd/dist/antd.css';
-import './App.scss';
+import "antd/dist/antd.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
       <LandingSection />
+      <AboutSection />
+      <SkillsSection />
     </div>
   );
 }
