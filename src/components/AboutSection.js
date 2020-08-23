49c73aff21e2b5921847bf39c98a21e51ab70a6c
Nam Phan <namtphann@gmail.com>
2020-08-24 00:48:41 +0200

    About me text updated

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -26,13 +26,14 @@ const AboutSection = () => (
     >
       <H1>Hello! I'm Nam. Nice to meet you.</H1>
       <H2>
-        I'm currently a senior software engineering student at the Amsterdam 
-        University of Applied Sciences. I got interested in software engineering 
-        because of the impact of software in our daily lives and the cool things 
-        that you could build. Currently I am more of a Front-End developer, 
-        but long-term I intend to move towards becoming a Full Stack developer. I
-        also enjoy experimenting with new technologies and meeting new people from all
-        over the world. 
+        I'm currently a 4th year software engineering student at the Amsterdam
+        University of Applied Sciences. I'm a web and mobile app developer.
+        Currently I am more of a Front-End developer, but long-term I intend to
+        move towards becoming a Full Stack developer. I'm also a flexible and
+        organized team player. I also like to meet or work with people from all
+        over the world to learn more about different cultures. In my spare time
+        I like to work on my own projects or watching videos about new tech and
+        gadgets.
       </H2>
     </Col>
   </Row>
