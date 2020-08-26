9b5071144dbc1ae0edc42c3d1f8cde6ce848fcda
Nam Phan <namtphann@gmail.com>
2020-08-27 01:21:28 +0200

    WIP: adding experience

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -2,12 +2,21 @@ import React from "react";
 import { Row, Col } from "antd";
 import styled from "styled-components";
 
+import Logo from "../assets/logo.png"
+
 const ContactSection = () => (
   <Row>
-    <Col>
-    
+    <Col
+      span={24}
+      style={{
+        backgroundColor: "#1890ff",
+        padding: "9rem 1.5rem",
+        textAlign: "center",
+      }}
+    >
+      
     </Col>
   </Row>
-)
+);
 
-export default ContactSection
+export default ContactSection;
--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Row, Col, Tabs } from "antd";
 import styled from "styled-components";
 
+import ExperienceData from "../data/experience.json";
+import EducationData from "../data/education.json";
+
 const { TabPane } = Tabs;
 
 const H1 = styled.h1`
@@ -11,13 +14,57 @@ const H1 = styled.h1`
   text-align: center;
 `;
 
+const Span = styled.span`
+  font-size: 15px;
+  font-weight: 500;
+  color: #141c3a;
+`;
+
+const Url = styled.a`
+  color: #1890ff;
+`;
+
+const ExpBlock = (props) => (
+  <Col
+    key={props.id}
+    xs={24}
+    sm={24}
+    md={12}
+    lg={12}
+    xl={12}
+    style={{ textAlign: "left", padding: "20px" }}
+  >
+    <Span>
+      {props.title}{" "}
+      <Url href={props.website} target="_blank">
+        @ {props.company}{" "}
+        <img
+          src={require("../assets/flags/" + props.country + ".png")}
+          width="22"
+          height="16"
+        />
+      </Url>
+    </Span>
+  </Col>
+);
+
 const ExperienceSection = () => (
-  <Row style={{ marginTop: "80px" }}>
+  <Row style={{ margin: "80px 0px" }}>
     <Col span={24}>
       <H1>Experience & Education</H1>
-      <Tabs defaultActiveKey="1" centered style={{textAlign: "center"}}>
+      <Tabs defaultActiveKey="1" centered style={{ textAlign: "center" }}>
         <TabPane tab="Experience" key="1">
-          Content of Tab Pane 1
+          <Row>
+            {ExperienceData.map((obj) => (
+              <ExpBlock
+                id={obj.id}
+                title={obj.title}
+                company={obj.company}
+                website={obj.website}
+                country={obj.country}
+              />
+            ))}
+          </Row>
         </TabPane>
         <TabPane tab="Education" key="2">
           Content of Tab Pane 2
