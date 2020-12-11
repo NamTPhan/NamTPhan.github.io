f4a34f0679003a8fd668c895023a4e26b021b1b7
Nam <namtphann@gmail.com>
2020-12-11 23:47:24 +0100

    Infinite scroll fix

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Portfolio website V5
+
+### Libraries
+
+- [styled-components](https://styled-components.com/)
+- [antd](https://ant.design/)
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -4,5 +4,7 @@ export const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.body};
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `
