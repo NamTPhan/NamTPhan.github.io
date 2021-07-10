4a60e80d9232559eb0f0d8f055a1ff1eaa90cbe5
Nam <namtphann@gmail.com>
2021-07-10 16:49:15 +0200

    Update and revert some changes

---
+++ build/precache-manifest.d143abcf341bae92580c01b121a05e61.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a88b82dc8772f33f62634dfc617130",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9e4fb62799c7db29992",
+    "url": "/static/css/2.3c09dbcf.chunk.css"
+  },
+  {
+    "revision": "c9e4fb62799c7db29992",
+    "url": "/static/js/2.7ef28988.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/static/js/2.7ef28988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ae0aefaa387345f293",
+    "url": "/static/js/main.c3c0bdf8.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "2780977f56c0c1caf57c5c46f3ab9131",
+    "url": "/static/media/avatar.2780977f.svg"
+  },
+  {
+    "revision": "32c19fbf06361e6ebae5aa5434dbd656",
+    "url": "/static/media/countrybucketlist.32c19fbf.png"
+  },
+  {
+    "revision": "38ba64cb180bd5fc43aeaf7407ded2fb",
+    "url": "/static/media/easyworx.38ba64cb.png"
+  },
+  {
+    "revision": "9e0aebbb6dadbed505188f5ca412c51d",
+    "url": "/static/media/laptop.9e0aebbb.svg"
+  },
+  {
+    "revision": "5039864cceefa1c9cf8f8a3aebbbd405",
+    "url": "/static/media/moon.5039864c.svg"
+  },
+  {
+    "revision": "dae868c68aa40ce1466cf447ae859118",
+    "url": "/static/media/mystudyprogress.dae868c6.png"
+  },
+  {
+    "revision": "e1bb2cc73021b3967272fca53a84621f",
+    "url": "/static/media/sun.e1bb2cc7.svg"
+  },
+  {
+    "revision": "bef28c277fb21194e917c380fec8ca90",
+    "url": "/static/media/sustainablechoice.bef28c27.png"
+  }
+]);
